fdc0c4187fb5b3870b466d5ca996147568594419
yunsoo1 <shin4895@gmail.com>
2022-07-01 11:39:24 +0900

    Update firebase.js

--- twitteryun/src/firebase.js
+++ twitteryun/src/firebase.js
@@ -4,12 +4,12 @@ import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCbKdzo35ifL60b7p_gmvBZpp2OG3c0aW0",
-  authDomain: "twitteryun-4395f.firebaseapp.com",
-  projectId: "twitteryun-4395f",
-  storageBucket: "twitteryun-4395f.appspot.com",
-  messagingSenderId: "878944811420",
-  appId: "1:878944811420:web:427fd4fb28bc7c0f3ff059"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
